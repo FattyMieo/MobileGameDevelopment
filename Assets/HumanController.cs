85e459f9d962b992f525afc785a0cf7509687451
FattyMieo <fong2009@live.com.my>
2017-10-05 18:28:04 +0800

    Locomotion

---
+++ Assets/HumanController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanController : MonoBehaviour
+{
+	Animator anim;
+	public Transform ikPoi; //Point of interest
+	public Transform ikHintPoi;
+	public float ikPosWeight;
+
+	// Use this for initialization
+	void Start ()
+	{
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void OnAnimatorIK (int layerIndex)
+	{
+		anim.SetIKPosition(AvatarIKGoal.RightHand, ikPoi.position);
+		anim.SetIKPositionWeight(AvatarIKGoal.RightHand, ikPosWeight);
+		anim.SetIKHintPosition(AvatarIKHint.RightElbow, ikHintPoi.position);
+		anim.SetIKHintPositionWeight(AvatarIKHint.RightElbow, 1f);
+	}
+
+	public void PlayFootstepSFX()
+	{
+		Debug.Log("PlayFootstepSFX");
+	}
+}
