09394fe75a5a0cf4c0456fa1309d5f76f1fd9ab6
FattyMieo <fong2009@live.com.my>
2017-10-12 17:56:23 +0800

    NavMesh

---
+++ Assets/CapsuleNavAI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CapsuleNavAI : MonoBehaviour
+{
+	NavMeshAgent navMeshAgent;
+	public Transform target;
+
+	// Use this for initialization
+	void Start ()
+	{
+		navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+		StartCoroutine("RepathRoutine");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	IEnumerator RepathRoutine()
+	{
+		while(true)
+		{
+			yield return new WaitForSeconds(0.5f);
+			navMeshAgent.SetDestination(target.position);
+		}
+	}
+}
---
+++ Assets/Editor/NavPathGizmo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.AI;
+
+public class NavPathGizmo : Editor
+{
+	[DrawGizmo(GizmoType.Selected | GizmoType.NonSelected)]
+	static void DrawNavPath(NavMeshAgent nav, GizmoType gizmoType)
+	{
+		Gizmos.color = Color.red;
+
+		Vector3[] points = nav.path.corners;
+
+		for(int i = 0; i < points.Length - 1; i++)
+		{
+			Gizmos.DrawLine(points[i], points[i+1]);
+		}
+	}
+}
