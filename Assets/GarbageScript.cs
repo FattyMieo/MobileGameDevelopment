a1e3318ae04e6d17be2a2f3a5f90df2c8b77affc
FattyMieo <fong2009@live.com.my>
2017-11-06 17:04:37 +0800

    Optimization Basics

---
+++ Assets/GarbageScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarbageScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Transform[] tArray = GameObject.FindObjectsOfType<Transform>();
+
+//		foreach(Transform t in tArray)
+//		{
+//			Debug.Log(string.Format("{0}", t.name));
+//		}
+	}
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,39 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//
+//public class Test : MonoBehaviour {
+//
+//	//Caching
+//	public Transform _transform;
+//	public Renderer _rend;
+//
+//	// Use this for initialization
+//	void Start ()
+//	{
+//		//Caching
+//		this._transform == this.GetComponent<Transform>();
+//		this._rend == this.GetComponent<MeshRenderer>();
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		if(posX != 1f) //Call function when necessary only
+//		{
+//			RandomFunction();
+//		}
+//
+//		List<GameObject> stuff = new List<GameObject>();
+//		stuff.Clear(); // Clear collections
+//
+//		Debug.Log(string.Format("??? {0}", gameObject.name)); //Boxing, don't do this
+//	}
+//
+//	IEnumerator Sequence()
+//	{
+//		float delay = 1f;
+//
+//		yield return delay;
+//		yield return new WaitForSeconds(1); //Don't do this
+//	}
+//}
