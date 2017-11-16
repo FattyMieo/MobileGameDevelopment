639eb3b10d551cf56badc52798c5fec8020465a3
FattyMieo <fong2009@live.com.my>
2017-11-16 19:05:16 +0800

    Cinemachine, TImeline, ImageEffects

---
+++ Assets/Wrapper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+public class Wrapper : MonoBehaviour {
+
+	public Vortex vortex;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 screenSpacePoint = Camera.main.WorldToViewportPoint(this.transform.position);
+		vortex.center = new Vector2(screenSpacePoint.x, screenSpacePoint.y);
+	}
+}
